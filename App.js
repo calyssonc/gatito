a9d1f07c7358b2d5ca79a2dffb596e80bdff2e49
calyssonc <calyssonc@gmail.com>
2021-06-01 13:49:30 -0300

    app finalizado

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import TelaPadrao from './src/componentes/TelaPadrao';
+import Reactotron from 'reactotron-react-native';
 
-import Servicos from './src/telas/Servicos';
-import Carrinho from './src/telas/Carrinho';
 import Rotas from './src/Rotas';
 
+//Para debugar o projeto
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
+
 export default function App() {
-  return <Rotas/>;
+  console.log('teste');
+  return <TelaPadrao><Rotas/></TelaPadrao>;
 }
